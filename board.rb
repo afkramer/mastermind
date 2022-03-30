abe13f90a7a24c8cb8acf64489089ec68953236a
Adriana Kramer <adri.kramer@gmail.com>
2022-03-30 21:34:23 +0200

    Add pseudocode to game class and colorize info in board class

--- board.rb
+++ board.rb
@@ -1,1 +1,21 @@
 # board.rb
+
+# Stack overflow question:
+# https://stackoverflow.com/questions/1489183/how-can-i-use-ruby-to-colorize-the-text-output-to-a-terminal
+
+# Info about getting gems to work with replit:
+# https://replit.com/talk/announcements/Ruby-Packages-via-RubyGems/11349
+
+require 'colorize'
+
+class Board
+  def initialize
+    @codepegs
+    @keypegs
+  end
+end
+
+# test colorize
+require 'colorize'
+
+puts String.color_samples
