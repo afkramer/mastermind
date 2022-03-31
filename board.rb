02f34ef50ada7402e00aed9363f0d0b86c9ed3a4
Adriana Kramer <adri.kramer@gmail.com>
2022-03-31 19:32:53 +0200

    Add functions to store codepeg and keypeg arrays including colorizer

--- board.rb
+++ board.rb
@@ -8,14 +8,51 @@
 
 require 'colorize'
 
+# Board holds guess and key arrays to be displayed in GUI
+# Colorize is used to add color to the pegs
 class Board
+  attr_reader :codepegs, :keypegs
+
   def initialize
-    @codepegs
-    @keypegs
+    @codepegs = []
+    @keypegs = []
+  end
+
+  def add_guess(guess_array)
+    guess_array.map! do |peg|
+      add_code_color(peg)
+    end
+
+    @codepegs.push(guess_array)
+  end
+
+  def add_keys(key_array)
+    key_array.map! do |peg|
+      add_key_color(peg)
+    end
+
+    @keypegs.push(key_array)
+  end
+
+  def add_code_color(peg)
+    case peg
+    when 'r'
+      'O'.colorize(:red)
+    when 'b'
+      'O'.colorize(:light_cyan)
+    when 'g'
+      'O'.colorize(:green)
+    when 'y'
+      'O'.colorize(:yellow)
+    end
+  end
+
+  def add_key_color(peg)
+    case peg
+    when 'r'
+      'o'.colorize(:light_red)
+    when 'w'
+      'o'.colorize(:light_white)
+    end
   end
 end
-
-# test colorize
-require 'colorize'
-
-puts String.color_samples
