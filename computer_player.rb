26da5b30af91d29bc31d3adb30b8291d6c35602a
Adriana Kramer <adri.kramer@gmail.com>
2022-04-03 19:09:22 +0200

    Add methods to initialize all possible guesses in computer_player

--- computer_player.rb
+++ computer_player.rb
@@ -1,14 +1,19 @@
 # computer_player.rb
 
-# TEST
-
 require './game_constants'
 require './player'
 
 # Contains the computer-specific methods for playing the game
 class ComputerPlayer < Player
+  attr_reader :possible_guesses
+  
   include GameConstants
 
+  def initialize
+    super
+    @possible_guesses = []
+  end
+  
   def set_name
     @name = @gui.get_computer_name
   end
@@ -18,7 +23,7 @@ class ComputerPlayer < Player
     @master_code
   end
 
-  def rate_guess(guess_array)
+  def rate_guess(master_array, guess_array)
     master_copy = @master_code.clone
     guess_copy = guess_array.clone
     keypeg_array = []
@@ -40,4 +45,45 @@ class ComputerPlayer < Player
     end
     keypeg_array
   end
+
+  def initialize_possible_guesses
+    GameConstants::COLORS.each do |pos_one|
+      GameConstants::COLORS.each do |pos_two|
+        GameConstants::COLORS.each do |pos_three|
+          GameConstants::COLORS.each do |pos_four|
+            @possible_guesses.push([pos_one, pos_two, pos_three, pos_four])
+          end
+        end
+      end
+    end
+  end
+
+  def initialize_possible_guesses_better
+    # GameConstants::COLORS.permutation(4) { |perm| @possible_guesses.push(perm) }
+    colors_to_permutate = []
+    GameConstants::COLORS.cycle(4) { |color| colors_to_permutate.push(color) }
+    p colors_to_permutate.sort
+    colors_to_permutate.sort.permutation(4) { |perm| @possible_guesses.push(perm) }
+    @possible_guesses.uniq!
+  end
+
+  # Using Swazsek's method
+  def guess(guess_num, *keypegs)
+    if guess_num == 1
+      %w[r r g g]
+    else
+      possible_guesses = possible_guesses.select do |guess_array|
+        rate_guess(guess_array, prev_guess) == keypegs
+      end
+    end
+  end
 end
+
+computer_player = ComputerPlayer.new
+# computer_player.initialize_possible_guesses
+computer_player.initialize_possible_guesses_better
+puts computer_player.possible_guesses.length
+p computer_player.possible_guesses[0]
+p computer_player.possible_guesses[1]
+p computer_player.possible_guesses[-2]
+p computer_player.possible_guesses[-1]
