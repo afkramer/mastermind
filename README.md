557af6a667ee4a7565e13e00ed5a7d049e303e62
Adriana Kramer <adri.kramer@gmail.com>
2022-04-03 20:54:17 +0200

    Update guess method in ComputerPlayer class

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # mastermind
 My solution to the Mastermind project in the Ruby path of The Odin Project.
 
-## Version 1
-As the game now stands, the computer player is always the codemaker and the human player is always the code breaker.
+## Implementation
+
+I decided to go with the Swaszek strategy, as I could picture how to create an array with all possible guesses and then reduce that array based on keypegs from previous guesses.
+
+https://puzzling.stackexchange.com/questions/546/clever-ways-to-solve-mastermind
+
+## What did I learn
+
+I learned about the permutation method of the Array class -- it took a few tries to use that to create the array of 1,296 possible code combinations. I think there is still some room for improvement here, however, as this version first creates a much larger array with lots of duplicates that then needs to be pared back to the 1,296 unique combinations. My original naive idea of setting up 4 nested loops and creating the arrays that way is actually a lot faster. However, since it only has to be done once at the beginning of the game I don't think it should be a problem.
--- computer_player.rb
+++ computer_player.rb
@@ -6,14 +6,14 @@ require './player'
 # Contains the computer-specific methods for playing the game
 class ComputerPlayer < Player
   attr_reader :possible_guesses
-  
+
   include GameConstants
 
   def initialize
     super
     @possible_guesses = []
   end
-  
+
   def set_name
     @name = @gui.get_computer_name
   end
@@ -23,65 +23,21 @@ class ComputerPlayer < Player
     @master_code
   end
 
-  def rate_guess(guess_array, master_array = @master_code)
-    master_copy = master_array.clone
-    guess_copy = guess_array.clone
-    keypeg_array = []
-
-    guess_copy.each_with_index do |peg, index|
-      if master_copy[index] == peg
-        keypeg_array.push('kr')
-        guess_copy[index] = 'x'
-        master_copy[index] = 'x'
-      end
-    end
-
-    guess_copy.each do |peg|
-      master_index = peg == 'x' ? nil : master_copy.find_index(peg)
-      if master_index
-        keypeg_array.push('kw')
-        master_copy[master_index] = 'x'
-      end
-    end
-    keypeg_array
-  end
-
   def initialize_possible_guesses
-    GameConstants::COLORS.each do |pos_one|
-      GameConstants::COLORS.each do |pos_two|
-        GameConstants::COLORS.each do |pos_three|
-          GameConstants::COLORS.each do |pos_four|
-            @possible_guesses.push([pos_one, pos_two, pos_three, pos_four])
-          end
-        end
-      end
-    end
-  end
-
-  def initialize_possible_guesses_better
     colors_to_permutate = []
     GameConstants::COLORS.cycle(4) { |color| colors_to_permutate.push(color) }
     colors_to_permutate.sort.permutation(4) { |perm| @possible_guesses.push(perm) }
     @possible_guesses.uniq!
   end
 
-  # Using Swazsek's method
-  def guess(guess_num, *keypegs)
+  def guess(guess_num, prev_guess = [], keypegs = [])
     if guess_num == 1
       %w[r r g g]
     else
-      possible_guesses = possible_guesses.select do |guess_array|
-        rate_guess(guess_array, prev_guess) == keypegs
+      @possible_guesses = @possible_guesses.select do |guess_array|
+        new_keypegs = rate_guess(guess_array, prev_guess)
+        keypegs == new_keypegs
       end
     end
   end
 end
-
-computer_player = ComputerPlayer.new
-# computer_player.initialize_possible_guesses
-computer_player.initialize_possible_guesses_better
-puts computer_player.possible_guesses.length
-p computer_player.possible_guesses[0]
-p computer_player.possible_guesses[1]
-p computer_player.possible_guesses[-2]
-p computer_player.possible_guesses[-1]
